d2d836747c38deed84ff90ffb3a52c5fc61846f5
胡剑峰2020 <952405831@qq.com>
2020-11-11 17:02:29 +0800

    消息队列太难了

--- admin/src/main/java/com/hujf/project/mq/SpringProducer.java
+++ admin/src/main/java/com/hujf/project/mq/SpringProducer.java
@@ -1,29 +1,30 @@
-package com.hujf.project.mq;
-
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-
-/**
- * @author Hujf
- * @title: SpringProducer
- * @date 2020/11/11 0011上午 10:15
- * @description: spring生产者
- */
-
-@Component
-public class SpringProducer {
-    //注入rocket
-    @Resource
-    private RocketMQTemplate template;
-
-    /**
-     * 发送消息
-     * @param topic
-     * @param msg
-     */
-    public void sendMsg(String topic,String msg){
-        this.template.convertAndSend(topic,msg);
-    }
-}
+//package com.hujf.project.mq;
+//
+//import org.apache.rocketmq.spring.core.RocketMQTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * @author Hujf
+// * @title: SpringProducer
+// * @date 2020/11/11 0011上午 10:15
+// * @description: spring生产者
+// */
+//
+//@Component
+//public class SpringProducer {
+//    //注入rocket
+//    @Autowired(required=true)
+//    private RocketMQTemplate template;
+//
+//    /**
+//     * 发送消息
+//     * @param topic
+//     * @param msg
+//     */
+//    public void sendMsg(String topic,String msg){
+//        this.template.convertAndSend(topic,msg);
+//    }
+//}
